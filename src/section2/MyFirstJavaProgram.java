731e295449fc519a06158fdf8d51f319c523d33d
League Student <league@tests-iMac.attlocal.net>
2019-07-09 14:30:01 -0700

    Day 2
    :)

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+Robot rob= new Robot();	
+rob.setSpeed(50);
+rob.penDown();
+	for (int i = 0; i < 4; i++) {
+		rob.move(100);
+		rob.turn(90);
 		
+	}
+	
 		
 	}
 }
--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -10,32 +10,36 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+		Robot bob = new Robot();
 		// 3. Put the robot's pen down
-
+		bob.penDown();
 		// 8. Make the robot go at maximum speed (100)
-
+		bob.setSpeed(100);
 		// 9. Set the pen to a color that you like for the shape
-
+		bob.setPenColor(225, 0, 0);
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
-		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-
-		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
+		int sides = 6;
+		// 5. Make a second variable for the angle you want the robot to turn. Hint: you
+		// can divide in Java using "/". Can’t test until step 6
+		int dist = 360 / sides;
+		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you
+		// should see a pentagon
+		for (int i = 0; i < 200; i++) {
 
 			// 2. Move the robot 200 pixels
-	
-			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
-	
+			bob.move(i);
+			// 10. Change the previous line of code to make the robot move "i" pixels
+			// instead of 200
 			// 6. Turn the robot the amount in your angle variable
-	
+			bob.turn(dist);
 			// 11. Turn the robot one more degree
-
+			bob.turn(1);
+		}
 	}
 
 	// Variations:
 	// *12. make the pattern really huge
-	// *13. randomize the color of the pattern 
+	// *13. randomize the color of the pattern
 	// *14. experiment with different shapes
 
 	public static void main(String[] args) {
